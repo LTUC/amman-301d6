afdddd893675b4e17d2fa36dbb2b89abf404a6ea
hanaa maratouk <hanaamaratouk@gmail.com>
2020-07-22 14:52:04 +0300

    class4-demo

--- class-01/challenges/challenges-01.test.js
+++ class-01/challenges/challenges-01.test.js
@@ -10,10 +10,12 @@ Then, write a function named speaker that takes in a string and a callback funct
 
 const greeting = (word) => {
   // Solution code here...
+  word.toUpperCase();
 };
 
 const speaker = (message, callback) => {
   // Solution code here...
+  callback(message);
 };
 
 /* ------------------------------------------------------------------------------------------------
